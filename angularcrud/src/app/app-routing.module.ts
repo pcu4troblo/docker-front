403f1e8fd58494bab07f77126fb9833b913bafed
fredy <fredy.arroyave@udea.edu.co>
2019-09-17 13:52:22 -0500

    update

---
+++ angularcrud/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CrudAddComponent } from './crud-add/crud-add.component';
+import { CrudEditComponent } from './crud-edit/crud-edit.component';
+import { CrudGetComponent } from './crud-get/crud-get.component';
+const routes: Routes = [
+{
+path: 'business/create',
+component: CrudAddComponent
+},
+{
+path: 'business/edit/:id',
+component: CrudEditComponent
+},
+{
+path: 'business',
+component: CrudGetComponent
+}
+];
+@NgModule({
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule]
+})
+export class AppRoutingModule {}
